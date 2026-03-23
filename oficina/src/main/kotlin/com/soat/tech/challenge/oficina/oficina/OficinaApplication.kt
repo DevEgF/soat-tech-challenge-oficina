842aa7e5658f8aebd099e646db85203b6173abe6
J Egito GS Filho <egitofilho4@gmail.com>
2026-03-23 20:22:51 -0300

    first commit

---
+++ oficina/src/main/kotlin/com/soat/tech/challenge/oficina/oficina/OficinaApplication.kt
@@ -0,0 +1,11 @@
+package com.soat.tech.challenge.oficina.oficina
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OficinaApplication
+
+fun main(args: Array<String>) {
+	runApplication<OficinaApplication>(*args)
+}
